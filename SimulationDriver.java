d4270587a49665c6b5f1a81c3e927570b01168d5
kiennguyenGH <ktnguyen4647@gmail.com>
2022-09-29 10:02:28 -0700

    Finalize main

--- SimulationDriver.java
+++ SimulationDriver.java
@@ -26,7 +26,7 @@ public class SimulationDriver {
        
 
         String[] tempAnswers2 = {"1","2","3","4","5"};
-        MultipleChoiceQuestion question2 = new MultipleChoiceQuestion("How big is the number 5?", tempAnswers2);
+        MultipleChoiceQuestion question2 = new MultipleChoiceQuestion("How many digits is the number 5?", tempAnswers2);
         question2.displayQuestion();
         
         VotingService iVote2 = new VotingService(students, question2);
@@ -36,6 +36,42 @@ public class SimulationDriver {
         }
         iVote2.getMultipleChoiceSubmissions();
         iVote2.displayMultipleAnswers();
+
+        String[] tempAnswers3 = {"1984","Lord of the Flies","Fahrenheit 451","Animal Farm"};
+        MultipleChoiceQuestion question3 = new MultipleChoiceQuestion("Which books did George Orwell write?", tempAnswers3);
+        question3.displayQuestion();
+        
+        VotingService iVote3 = new VotingService(students, question3);
+        for (Student i : students.values())
+        {
+            i.generateMultipleChoiceAnswer(question3);
+        }
+        iVote3.getMultipleChoiceSubmissions();
+        iVote3.displayMultipleAnswers();
+
+        String[] tempAnswers4 = {"red","yellow","blue"};
+        MultipleChoiceQuestion question4 = new MultipleChoiceQuestion("What are the possible colors of the sky?", tempAnswers4);
+        question4.displayQuestion();
+        
+        VotingService iVote4 = new VotingService(students, question4);
+        for (Student i : students.values())
+        {
+            i.generateMultipleChoiceAnswer(question4);
+        }
+        iVote4.getMultipleChoiceSubmissions();
+        iVote4.displayMultipleAnswers();
+
+        String[] tempAnswers5 = {"True","False"};
+        SingleChoiceQuestion question5 = new SingleChoiceQuestion("What is the shape of the Earth?", tempAnswers5);
+        question5.displayQuestion();
+        
+        VotingService iVote5 = new VotingService(students, question2);
+        for (Student i : students.values())
+        {
+            i.generateSingleChoiceAnswer();
+        }
+        iVote5.getSingleChoiceSubmissions();
+        iVote5.displayAnswers();
 
     }
 }
