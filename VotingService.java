c15f83f94b69c094435d4f16a62206ee92b39f8c
kiennguyenGH <ktnguyen4647@gmail.com>
2022-09-28 20:26:12 -0700

    Add hashmap for Voting Service

--- VotingService.java
+++ VotingService.java
@@ -1,15 +1,47 @@
+import java.util.HashMap;
 
 public class VotingService{
-
-    private int[] answers;
+    
+    private int[] singleAnswer = {0, 0};
+    private int[] multipleAnswer;
     private int numAnswers;
+    private SingleChoiceQuestion singleChoiceQuestion;
+    private MultipleChoiceQuestion multipleChoiceQuestion;
+    private HashMap<String, Student> studentList = new HashMap<String, Student>();
 
+    public VotingService(HashMap list, SingleChoiceQuestion question)
+    {
+        studentList = list;
+        singleChoiceQuestion = question;
+        numAnswers = question.getNumAnswers();
+    }
+
+    public VotingService(HashMap list, MultipleChoiceQuestion question)
+    {
+        studentList = list;
+        multipleChoiceQuestion = question;
+        numAnswers = question.getNumAnswers();
+        multipleAnswer = new int[question.getNumAnswers()];
+        for (int i = 0; i < multipleAnswer.length; i++)
+        {
+            multipleAnswer[i] = 0;
+        }
+    }
+
+    public void getSingleChoiceSubmissions()
+    {
+        for (Student student: studentList.values())
+        {
+            singleAnswer[student.getSingleChoiceAnswer()-1]++;
+        }
+    }
+    
     public int getNumAnswers()
     {
         return numAnswers;
     }
 
-    public void displayAnswers()
+    public void displayAnswers(int[] answers)
     {
         for (int i = 0; i < answers.length; i++)
         {
